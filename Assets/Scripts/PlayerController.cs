d6ac84e01cc07c2d5401a9574891540b59f25862
Lauriane <lauriane.loas@gmail.com>
2018-11-07 23:13:16 +0100

    Run And Jump

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+[RequireComponent(typeof(PlayerMotor))]
+
+public class PlayerController : MonoBehaviour {
+
+    private PlayerMotor motor;
+	// Use this for initialization
+	void Start () {
+        motor = GetComponent<PlayerMotor>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float _x = Input.GetAxisRaw("Horizontal");
+        float _y = Input.GetAxisRaw("Jump");
+
+        Vector2 _velocity = new Vector2(_x, _y);
+
+        Debug.Log("vecteur : " + _velocity);
+
+        motor.RunAndJump(_velocity);
+    }
+}
---
+++ Assets/Scripts/PlayerMotor.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(PlayerController))]
+public class PlayerMotor : MonoBehaviour {
+
+    private Vector2 velocity;
+    private Rigidbody2D rb;
+
+    [SerializeField]
+    private float maxSpeed, maxSpeedJump, radiusCircle;
+
+    [SerializeField]
+    private GameObject groundCheck;
+
+    [SerializeField]
+    private LayerMask layer;
+
+    // Use this for initialization
+    void Start() {
+        velocity = Vector2.zero;
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate() {
+        RunAndJumpPerform();
+    }
+
+    public void RunAndJump(Vector2 _velocity){
+        velocity = _velocity;
+    }
+
+    private void RunAndJumpPerform()
+    {
+        bool _grounded = Physics2D.OverlapCircle(groundCheck.transform.position,radiusCircle, layer);
+        if(_grounded)
+        {
+            rb.AddForce(new Vector2(0f, velocity.y) * Time.deltaTime * maxSpeedJump, ForceMode2D.Impulse);
+        }
+
+
+        rb.velocity = new Vector2(velocity.x * maxSpeed * Time.deltaTime, rb.velocity.y);
+    }
+
+    public void OnDrawGizmos()
+    {
+        Gizmos.DrawSphere(groundCheck.transform.position, radiusCircle);
+    }
+
+}
